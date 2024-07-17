68fe7fa74bb9272f9d0b0174b8142e62ff1522c3
khusrow <khusrow.imamyar@gmail.com>
2024-07-17 13:20:33 +0530

    created react crud app

---
+++ crud-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Table from './components/Table';
+import Parent from './components/Parent';
+
+function App() {
+  return (
+    <div className="App">
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
---
+++ crud-app/src/components/Parent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Parent() {
+
+// all states
+// api call
+
+  return <div>My Parent</div>;
+  //table // pass props
+  //search // pass props
+  //add record // pass props
+}
+
+export default Parent;
